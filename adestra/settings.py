d9af89be555b4b22a3c53ff14ee3fad87461bddd
rifat977 <itscrifat5147@gmail.com>
2024-12-14 00:58:10 +0600

    added mysql

--- adestra/settings.py
+++ adestra/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'adestra.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cparobust',
-        'USER': 'user1',
-        'PASSWORD': 'password1',
+        'NAME': 'shambhdn_cparobust',
+        'USER': 'shambhdn_cparobust',
+        'PASSWORD': 'Cparobust@2024',
         'HOST': 'localhost', 
         'PORT': '3306',     
         'OPTIONS': {
