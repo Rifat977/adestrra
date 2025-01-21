e6e3818c1acd42680fa7e6c0f891a4a9193bc232
rifat977 <itscrifat5147@gmail.com>
2025-01-22 02:02:12 +0600

    fix issue

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.html import strip_tags
+
 
 register = template.Library()
 
@@ -9,3 +11,15 @@ def dict_key(value, key):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, [])
+
+@register.filter
+def truncate_html(value, limit=150):
+
+    if not isinstance(value, str):
+        return value
+
+    plain_text = strip_tags(value)
+
+    if len(plain_text) > limit:
+        return f"{plain_text[:limit].rstrip()}..."
+    return plain_text
